494d3a07bd932c654a2b861671c96effea635f88
rdoering <rdoering.info@gmail.com>
2016-10-25 23:17:40 +0200

    Removed unused velocity

--- lib/actor.dart
+++ lib/actor.dart
@@ -2,7 +2,6 @@ part of snake;
 
 abstract class Actor extends Scenery {
   num _angle;
-  num _velocity;
 
   Actor(_game, _screen, _location, _size, _image, this._angle) :
     super(_game, _screen, _location, _size, _image);
